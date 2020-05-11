55bebcfefdad5145e55a840a3c22c8e19457f4e1
kokily <kokily@naver.com>
2020-05-11 20:17:05 +0900

    User Login, Logout, check User API

--- src/api/auth/auth.ctrl.ts
+++ src/api/auth/auth.ctrl.ts
@@ -37,3 +37,72 @@ export const register = async (ctx: Context) => {
     ctx.throw(500, err);
   }
 };
+
+// Local Login (POST) /api/auth/login
+export const login = async (ctx: Context) => {
+  const { username, password } = ctx.request.body;
+
+  if (!username || !password) {
+    ctx.status = 400;
+    ctx.body = '이름 또는 비밀번호를 입력하세요!';
+    return;
+  }
+
+  const data: ObjectSchema = Joi.object().keys({
+    username: Joi.string().required(),
+    password: Joi.string().min(4).required(),
+  });
+
+  const result: ValidationResult<string> = Joi.validate(ctx.request.body, data);
+
+  if (result.error) {
+    ctx.status = 400;
+    console.log(result.error);
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const valid = await user.checkPassword(password);
+
+    if (!valid) {
+      ctx.status = 400;
+      return;
+    }
+
+    ctx.body = user.serialize();
+
+    const token = user.generateToken();
+
+    ctx.cookies.set('__PAYSYS_AUTH__', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
+
+// Logout (POST) /api/auth/logout
+export const logout = async (ctx: Context) => {
+  ctx.cookies.set('__PAYSYS_AUTH__');
+  ctx.status = 204;
+};
+
+// User check (GET) /api/auth/check
+export const check = async (ctx: Context) => {
+  const { user } = ctx.state;
+
+  if (!user) {
+    ctx.status = 401;
+    return;
+  }
+
+  ctx.body = user;
+};
--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,5 +4,8 @@ import * as authCtrl from './auth.ctrl';
 const auth = new Router();
 
 auth.post('/register', authCtrl.register);
+auth.post('/login', authCtrl.login);
+auth.post('/logout', authCtrl.logout);
+auth.get('/check', authCtrl.check);
 
 export default auth;
